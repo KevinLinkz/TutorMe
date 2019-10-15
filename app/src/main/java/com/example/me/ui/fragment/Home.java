f12680032f81607a3d7f1bc53046090b6974aa22
Kevin <kevinhertanto12@gmail.com>
2019-10-16 01:13:22 +0700

    TutorMe V.2

--- app/src/main/java/com/example/me/ui/fragment/Home.java
+++ app/src/main/java/com/example/me/ui/fragment/Home.java
@@ -5,18 +5,37 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.me.R;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageClickListener;
+import com.synnapps.carouselview.ImageListener;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Home extends Fragment {
-
+    private int[] mImages = new int[]{
+            R.raw.image5,
+            R.raw.image1,
+            R.raw.image2,
+            R.raw.image3,
+            R.raw.image4
+    };
+    private String[] mTitle = new String[]{
+            "Buah1",
+            "Buah2",
+            "Buah3",
+            "Buah4",
+            "Buah5"
+    };
+    private ViewGroup root;
 
     public Home() {
         // Required empty public constructor
@@ -27,11 +46,29 @@ public class Home extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_home, null);
+        root = (ViewGroup) inflater.inflate(R.layout.fragment_home, null);
 
         //SEARCH VIEW
-        SearchView searchView =  root.findViewById(R.id.srcHome);
+        SearchView searchView = root.findViewById(R.id.srcHome);
         searchView.setFocusable(false);
+
+
+        CarouselView crsView = root.findViewById(R.id.crd_promo);
+        crsView.setPageCount(mImages.length);
+        crsView.setImageListener(new ImageListener() {
+            @Override
+            public void setImageForPosition(int position, ImageView imageView) {
+                imageView.setImageResource(mImages[position]);
+
+            }
+        });
+        crsView.setImageClickListener(new ImageClickListener() {
+            @Override
+            public void onClick(int position) {
+                Toast.makeText(root.getContext(), mTitle[position], Toast.LENGTH_LONG).show();
+
+            }
+        });
 
         return root;
 //        return inflater.inflate(R.layout.fragment_home, container, false);
